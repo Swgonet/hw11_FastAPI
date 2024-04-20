f722e473e1d59d6d3994c6ac277adc791993c8cb
599xx D <599xxd@gmail.com>
2024-04-20 11:19:29 +0300

    add files

--- db.py
+++ db.py
@@ -17,30 +17,4 @@ def get_db():
     finally:
         db.close()
 
-# from fastapi import FastAPI, Depends, HTTPException, Query
-# from sqlalchemy.orm import Session, sessionmaker
-# from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
-# from models import Contacts
-# from schemas import ContactResponse, ContactCreateUpdate
-# from datetime import date, timedelta
-# from sqlalchemy import func
 
-# # Create an async engine
-# ASYNC_DATABASE_URL = 'postgresql+asyncpg://postgres:qwerty@localhost:5432/abc'
-# async_engine = create_async_engine(ASYNC_DATABASE_URL)
-
-# # Create async session factory
-# async_session_factory = sessionmaker(
-#     async_engine, expire_on_commit=False, class_=AsyncSession
-# )
-
-# app = FastAPI()
-
-# # Dependency to get the async database session
-# async def get_db():
-#     async with async_session_factory() as session:
-#         yield session
-
-# ...
-
-# # All your route handlers using async def
